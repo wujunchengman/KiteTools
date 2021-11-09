7a1d01627b27c701470800c908f897c3192caf5d
吴俊城 <wujunchengman@163.com>
2021-11-09 10:23:42 +0800

    添加项目文件。

---
+++ KiteTools.ECharts/OptionBuilder.cs
@@ -0,0 +1,14 @@
+﻿namespace KiteTools.ECharts
+{
+    public static class OptionBuilder
+    {
+        /// <summary>
+        /// 获得双Y轴折线图
+        /// </summary>
+        /// <returns></returns>
+        public static string GenerateDoubleYLineChart()
+        {
+            return "";
+        }
+    }
+}
